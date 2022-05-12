c85bc0ffb42bc6f4b4cf5ba00e48bebaffa6d258
san9min <92682815+san9min@users.noreply.github.com>
2022-05-12 17:37:58 +0900

    finish

--- convert.py
+++ convert.py
@@ -11,15 +11,13 @@ class TokenLabelConverter(object):
         # [GO] for the start token of the attention decoder. [s] for end-of-sentence token.
         self.SPACE = '[s]'
         self.GO = '[GO]'
-        #self.MASK = '[MASK]'
 
-        #self.list_token = [self.GO, self.SPACE, self.MASK]
         self.list_token = [self.GO, self.SPACE]
         self.character = self.list_token + list(character)
         
 
         self.dict = {word: i for i, word in enumerate(self.character)}
-        self.batch_max_length = 25 + len(self.list_token) # 이거 설명좀 해주세여!! 
+        self.batch_max_length = 25 + len(self.list_token)
 
     def encode(self, text):
         """ convert text-label into text-index.
@@ -28,14 +26,7 @@ class TokenLabelConverter(object):
         batch_text = torch.LongTensor(len(text), self.batch_max_length).fill_(self.dict[self.GO])
         
         for i, t in enumerate(text):
-            # 수정 이전
-            # c = [[char] for char in t[0]]
-            # txt = [self.GO] + list(c) + [self.SPACE]
-            # txt = [self.dict[char] if j == 0 or j == int(len(txt)-1) else self.dict[char[0]] for j,char in enumerate(txt)]
-            
-            # 수정 후 - 수정 전과 동작은 동일
-            txt = [self.GO] + list(t[0]) + [self.SPACE]
-            
+            txt = [self.GO] + list(t[0]) + [self.SPACE]            
             txt = [self.dict[char] for char in txt]
             
             batch_text[i][:len(txt)] = torch.LongTensor(txt)  # batch_text[:, 0] = [GO] token
