3a381730db5a61cd210c690ec7cacd4b60fe12d4
JiHyuk-Byun <81155467+JiHyuk-Byun@users.noreply.github.com>
2022-05-12 17:25:52 +0900

    Update coco_eval.py

--- datasets/coco_eval.py
+++ datasets/coco_eval.py
@@ -210,14 +210,13 @@ def evaluate(self):
     Run per image evaluation on given images and store results (a list of dict) in self.evalImgs
     :return: None
     '''
-    # tic = time.time()
-    # print('Running per image evaluation...')
+
     p = self.params
     # add backward compatibility if useSegm is specified in params
     if p.useSegm is not None:
         p.iouType = 'segm' if p.useSegm == 1 else 'bbox'
         print('useSegm (deprecated) is not None. Running {} evaluation'.format(p.iouType))
-    # print('Evaluate annotation type *{}*'.format(p.iouType))
+
     p.imgIds = list(np.unique(p.imgIds))
     if p.useCats:
         p.catIds = list(np.unique(p.catIds))
